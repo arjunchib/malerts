9bdf688909f805a813b9d7878947b0b67360d90d
smoon72 <sjm9219@gmail.com>
2017-09-08 22:36:27 -0400

    Added list files

---
+++ list.py
@@ -0,0 +1,19 @@
+red = ["North Springs", "Sandy Springs", "Dunwoody", "Medical Center"
+, "Buckhead", "Art Center", "Midtown", "North Avenue", "Civic Center",
+"Peachtree Center", "Five Points", "Garnett", "Oakland City",
+"Lakewood", "East Point", "College Park", "Airpirt"]
+
+yellow = ["Doravile", "Chamblee", "Brookhaven", "Oglethorpe",
+"Lenox", "Lindberghh Center", "Art Center", "Midtown", "North Avenue",
+"Civic Center", "Peachtree Center", "Five Points", "Garnett",
+"Oakland City", "Lakewood", "East Point", "College Park", "Airpirt"]
+
+blue = ["Indian Creek", "Kensington", "Avondale", "Decatur",
+"East Lake", "Edgewood", "Candler Park", "Inman Park", "Reynoldstown",
+"King Memorial", "Georgia State", "Five Points", "Dome",
+"Philips Areana", "CNN Center", "Vine City", "Ashby", "West Lake",
+"Hamilton E. Holmes"]
+
+green = ["Edgewood", "Candler Park", "Inman Park", "Reynoldstown",
+"King Memorial", "Georgia State", "Five Points", "Dome",
+"Philips Areana", "CNN Center", "Vine City", "Ashby", "Bankhead"]
