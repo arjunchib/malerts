72920894a1a08c205f2fbcf636ba9dd276316d4a
Arjun Chib <arjunpatrickchib@gmail.com>
2017-09-09 12:16:24 -0400

    routing logic

--- routing.py
+++ routing.py
@@ -1,31 +1,68 @@
+from datetime import datetime
+from pytz import timezone
+
+class Direction(Enum):
+    North = 1
+    South = 2
+    East = 3
+    West = 4
+
 def findRoute(origin, dest):
+    return findRouteHelper(origin, dest, [])
+
+def findRouteHelper(origin, dest, route):
     origin_lines = findLinesWithStation(origin)
     dest_lines = findLinesWithStation(dest)
+    intersects = origin_lines.intersection(dest_lines)
+    print(intersects)
     route = []
-    if origin_lines == dest_lines:
-        if len(origin_lines) == 1:
-            route.append("Take the " + origin_lines[0].name + " line " + findDirection(origin, dest, origin_lines[0]) + "bound towards " + dest)
+    if len(intersects) >= 1:
+        route.append({
+            lines: intersects,
+            direction: findDirection(origin, dest, min(intersects))
+        })
+        return route
+    elif len(intersects) == 0:
+        dest_line_name = min(dest_lines).name
+        transfer = min(origin_lines).transfers[dest_line_name]
+        route.append({
+            lines: origin_lines,
+            direction: findDirection(origin, transfer, min(intersects))
+        })
+        return findRouteHelper(transfer, dest, route)
 
 def findLinesWithStation(station):
-    lines = []
+    lines = set()
+    now_utc = datetime.now(timezone('UTC'))
+    now_eastern = now_utc.astimezone(timezone('US/Eastern'))
+    if now_eastern.hour < 5 and
+       now_eastern.hour > 1 and
+       now_eastern.minute > 30:
+        return lines
     if station in red:
-        lines.append(red)
+        if now_eastern.hour > 21
+            lines.add(red_night)
+        else:
+            lines.add(red)
     if station in gold:
-        lines.append(gold)
+        lines.add(gold)
     if station in green:
-        lines.append(green)
+        if now_eastern.hour > 21
+            lines.add(green_night)
+        else:
+            lines.add(green)
     if station in blue:
-        lines.append(blue)
+        lines.add(blue)
     return lines
 
 def findDirection(origin, dest, line):
     if line.name is 'red' or line.name is 'gold':
         if line.index(origin) < line.index(dest):
-            return "North"
+            return Direction.North
         else:
-            return "South"
+            return Direction.South
     else:
         if line.index(origin) < line.index(dest):
-            return "East"
+            return Direction.East
         else:
-            return "West"
+            return Direction.West
