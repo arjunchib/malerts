d748de5a0d27b9ed2d66fa3589a98234adb2518b
kashyap@codemettle.com <kashyap@codemettle.com>
2017-09-08 22:12:27 -0400

    Nothing to see here

--- send_sms.py
+++ send_sms.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC36f8a20c0d2f1b8d30bb5abdc3035997"
+account_sid = ""
 # Your Auth Token from twilio.com/console
-auth_token  = "4385d9446189414082c7f9bf86566fb1"
+auth_token  = ""
 
 client = Client(account_sid, auth_token)
 
